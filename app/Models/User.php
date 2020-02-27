a66bb14895fbd8c6f7e73b7ef5eb24d380cdef32
Setiawan Hu <setiawan@ti.ukdw.ac.id>
2020-02-28 00:25:48 +0700

    Move all models to Models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use App\Traits\OutputCreateable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, OutputCreateable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,6 +18,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password',
+    ];
+
+    /**
+     * Attributes that will be shown as output.
+     *
+     * @var array
+     */
+    protected $fields = [
+        'email', 'name'
     ];
 
     /**
